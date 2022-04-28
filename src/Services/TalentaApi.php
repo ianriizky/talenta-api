39e9c7b3cf315843852712ec4b2a55ad9e843b66
Septianata Rizky Pratama <ian.rizkypratama@gmail.com>
2022-04-29 00:01:53 +0700

    - Add method definition of employee module on facade's docblock class

--- src/Services/TalentaApi.php
+++ src/Services/TalentaApi.php
@@ -9,6 +9,16 @@ use Illuminate\Support\Traits\Macroable;
 use RuntimeException;
 
 /**
+ * @method \Illuminate\Http\Client\Response addEmployee(array $data = []) Create "/employee" POST request to the Talenta api.
+ * @method \Illuminate\Http\Client\Response getEmployeeByUserID(string $employee_id, array|string|null $query = null) Create "/employee/:employee_id" GET request to the Talenta api.
+ * @method \Illuminate\Http\Client\Response getAllEmployee(array|string|null array|string|null $query = null) Create "/employee" GET request to the Talenta api.
+ * @method \Illuminate\Http\Client\Response getEmployeeOnLeaveStatusByID(string $employee_id, array|string|null $query = null) Create "/employee/:employee_id/status" GET request to the Talenta api.
+ * @method \Illuminate\Http\Client\Response patchEmployee(string $user_id, array $data = []) Create "/employee/:user_id" PATCH request to the Talenta api.
+ * @method \Illuminate\Http\Client\Response postEmployeeInformalEducation(array $data = []) Create "/employee/informal-education/create" POST request to the Talenta api.
+ * @method \Illuminate\Http\Client\Response patchEmployeeInformalEducation(string $informal_education_id, array $data = []) Create "/employee/informal-education/:informal_education_id/update" PATCH request to the Talenta api.
+ * @method \Illuminate\Http\Client\Response getEmployeeInformalEducation(string $user_id, array $data = []) Create "/employee/informal-education/:user_id/list" GET request to the Talenta api.
+ * @method \Illuminate\Http\Client\Response getEmployeeInformalEducationDetail(string $user_id, string $informal_education_id, array $data = []) Create "/employee/informal-education/:user_id/detail/:informal_education_id" GET request to the Talenta api.
+ *
  * @see https://documenter.getpostman.com/view/12246328/TWDZHvj1
  */
 class TalentaApi
