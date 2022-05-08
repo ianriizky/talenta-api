4dda7d34ac663692cf69b801ae43f0a3fb8ea6d0
Septianata Rizky Pratama <ian.rizkypratama@gmail.com>
2022-05-08 13:32:29 +0700

    - Add missing $file parameter definition of postEmployeeInformalEducation() method on service and facade class

--- src/Services/TalentaApi.php
+++ src/Services/TalentaApi.php
@@ -39,7 +39,7 @@ use RuntimeException;
  * @method \Illuminate\Http\Client\Response getAllEmployee(array|string|null $query = null) Create "/employee" GET request to the Talenta api.
  * @method \Illuminate\Http\Client\Response getEmployeeOnLeaveStatusByID(string $employee_id, array|string|null $query = null) Create "/employee/:employee_id/status" GET request to the Talenta api.
  * @method \Illuminate\Http\Client\Response patchEmployee(string $user_id, array $data = []) Create "/employee/:user_id" PATCH request to the Talenta api.
- * @method \Illuminate\Http\Client\Response postEmployeeInformalEducation(array $data = []) Create "/employee/informal-education/create" POST request to the Talenta api.
+ * @method \Illuminate\Http\Client\Response postEmployeeInformalEducation(array $data = [], string|resource|null $file = null) Create "/employee/informal-education/create" POST request to the Talenta api.
  * @method \Illuminate\Http\Client\Response patchEmployeeInformalEducation(string $informal_education_id, array $data = []) Create "/employee/informal-education/:informal_education_id/update" PATCH request to the Talenta api.
  * @method \Illuminate\Http\Client\Response getEmployeeInformalEducation(string $user_id, array $data = []) Create "/employee/informal-education/:user_id/list" GET request to the Talenta api.
  * @method \Illuminate\Http\Client\Response getEmployeeInformalEducationDetail(string $user_id, string $informal_education_id, array $data = []) Create "/employee/informal-education/:user_id/detail/:informal_education_id" GET request to the Talenta api.
